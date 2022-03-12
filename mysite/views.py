0fd9fd98f4e54767b1afad995ddcd77d4869e340
alexsavinov <spell477@gmail.com>
2022-03-12 21:52:11 +0200

    python-django - lesson04 - homework

--- mysite/views.py
+++ mysite/views.py
@@ -1,2 +1,18 @@
+import json
+
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
+class UsersListCreateView(APIView):
+    def get(self, *args, **kwargs):
+        with open('users.json', encoding='utf-8') as file:
+            return Response(json.load(file))
+
+    def post(self, *args, **kwargs):
+        with open('users.json', 'r+', encoding='utf-8') as file:
+            users = json.load(file)
+            users.append(self.request.data)
+            file.seek(0)
+            json.dump(users, file)
+            return Response('created')
